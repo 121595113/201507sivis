b63a584185ee1cbc4fde108f9adf172e1dbeaa12
121595113@qq.com <121595113@qq.com>
2015-10-11 23:30:29 +0800

    添加导航结构

--- config.rb
+++ config.rb
@@ -11,13 +11,12 @@ javascripts_dir = "javascripts"
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded
 
-sourcemap = true
-
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
+sourcemap = true
 
 
 # If you prefer the indented syntax, you might want to regenerate this
@@ -25,3 +24,13 @@ line_comments = false
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+if environment == :production
+  sourcemap = false
+  output_style = :expanded
+end
+
+if environment == :development
+  sourcemap = false
+  output_style = :compressed
+end
